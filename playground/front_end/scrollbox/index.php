73f7acfd6c72d272695db65afbc4bc1b23ad706d
Gregory Daynes <gregdaynes@gmail.com>
2011-08-17 14:43:25 -0700

    Flexbox & Scrollbox
    added two new playground projects Flexbox - the UI written using CSS3
    flexbox (webkit only for this example) Scrollbox - the logic for
    scrolling the page (only chrome tested)  Next up Zoombox - zoom function
    on double click ComboBox - all the boxes combined into one

---
+++ playground/front_end/scrollbox/index.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<head>
+	<meta charset="utf-8">
+
+	<title></title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="stylesheet/less" type="text/css" href="./style.less">
+	
+	<script src="./less.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/mootools/1.3.2/mootools-yui-compressed.js"></script>
+	
+</head>
+<body>
+
+	<div class="world_map">
+	   <div class="wrapper">
+	   		<div class="page">
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/01.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/02.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/03.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/04.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/05.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/06.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/07.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/08.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/09.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/10.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/11.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/12.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/13.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/14.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/15.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/16.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/17.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/18.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/19.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/20.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/21.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/22.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/23.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/001/0/24.jpg">
+	    		</div>
+	    	</div>	    	
+	    </div>
+	    
+	    <div class="page">
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/01.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/02.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/03.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/04.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/05.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/06.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/07.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/08.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/09.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/10.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/11.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/12.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/13.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/14.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/15.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/16.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/17.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/18.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/19.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/20.jpg">
+	    		</div>
+	    	</div>
+	    	<div class="row">
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/21.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/22.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/23.jpg">
+	    		</div>
+	    		<div class="column">
+	    			<img src="../../page_resources/002/0/24.jpg">
+	    		</div>
+	    	</div>
+	    </div>
+	    
+	    </div>
+	</div>
+	
+	<script src="./mootools-more-1.3.2.1.js"></script>
+	<script src="./script.js"></script>
+</body>
+</html>
