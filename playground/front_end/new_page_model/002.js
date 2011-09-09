2bd8a38ad8e79e7a79f6a957e3da3ad008c95217
Gregory Daynes <gregdaynes@gmail.com>
2011-09-09 16:23:44 -0700

    Continuing work on the new page scaling thingy
    committing it now, even though it's broken. My work day is over, time to
    go home, and continue working on it.

---
+++ playground/front_end/new_page_model/002.js
@@ -0,0 +1,237 @@
+var sizeObject = {
+	'-0': { x: 2048, y: 2669 },
+	'-1': { x: 1638, y: 2135 },
+	'-2': { x: 1310, y: 1708 },
+	'-3': { x: 1048, y: 1366 },
+	'-4': { x:  838, y: 1093 },
+	'-5': { x:  670, y:  874 },
+	'-6': { x:  536, y:  699 },
+	'-7': { x:  429, y:  559 },
+	'-8': { x:  343, y:  447 },
+	'-9': { x:  274, y:  358 },
+};
+
+var pageSize = null,
+    body = null,
+    wrapper = null,
+    zoomLevel = null,
+    htmlBackgroundSize = false,
+    pages = null,
+    grid = null;
+
+var generateFunction = function() {	
+	
+	body = $(document).getElement('body');
+	wrapper = $('wrapper');
+	pages = wrapper.getChildren('div');
+	pageSize = sizeObject['-0']; // max size predefined
+    bodySize = body.getSize().x;
+    
+	// clean out each page
+	pages.each(function(page) {
+		page.empty();
+	});	
+	
+	// find smallest usable size
+	Object.some(sizeObject, function(subObj, key) {
+		
+		// find the smallest - make this better
+		if (subObj.x > (bodySize / 2)) {
+			pageSize = sizeObject[key];
+			zoomLevel = key;
+		} else {
+			console.log(key);
+			if (key = null) {
+				pageSize = sizeObject['-9'];
+			};
+			return true;
+		};
+		
+	});
+		
+	// size container to pages size
+	body.setStyles({
+		width: pageSize.x * 2,
+		height: pageSize.y
+	});
+	
+	// number of grid items to generate
+	gridX = Math.ceil(pageSize.x / 256);
+	gridY = Math.ceil(pageSize.y / 256);
+	
+	// size wrapper
+	wrapper.setStyles({
+		width: pageSize.x * 2
+	});
+	
+	// size page
+	pages.each(function(page) {
+		page.setStyles({
+			width: pageSize.x,
+			height: pageSize.y
+		});
+		
+
+	});
+	
+	var row = new Element('div', { 
+		styles: {
+			width: gridX * 256,
+		},
+		'class': 'row'
+	});
+	var col = new Element('div');
+	
+	pages.each(function(page) {
+
+		// generate grid
+		for(r=0;r<gridY;r++) {
+			
+			tmp = row.clone();
+			page.adopt(tmp);
+			
+			for(c=0;c<gridX;c++) {
+				tmp.adopt(col.clone().setProperties({
+					id: 'r'+r + '_c'+c,
+					'class': 'grid'
+				}));
+			}		
+		};
+	});
+	
+	grid = $(document).getElements('[class~=grid]');
+
+	// scale grid
+	scaler();
+
+	// call for images
+	imageRequest();	
+	
+	
+};
+
+var scaler = function() {    
+    scalePercentage = body.getSize().x / (pageSize.x * 2);
+    
+    scale = 256 * scalePercentage;
+    
+    // scale items
+    pages.each(function(page) {
+    	page.setStyles({
+    		width: (pageSize.x) * scalePercentage,
+    		height: (pageSize.y) * scalePercentage
+    	});
+    	
+    	console.log(page.getSize().x);
+    	
+    });
+    
+    grid.each(function(el) {
+    	el.setStyles({
+    		width: scale,
+    		height: scale
+    	});
+    });
+
+    body.setStyles({
+        width: (pageSize.x * 2) * scalePercentage,
+        height: pageSize.y * scalePercentage
+    });
+    
+    console.log(body.getSize().x);
+    
+    
+    
+    // when finished scaling for x seconds
+    // check scale percentage
+    
+    // if percentage is < 80% or > 100%
+    // fetch new images
+    
+   	// else don't do anything
+    
+    if (scalePercentage < 1 && scalePercentage > 0.8) {
+        
+        
+    } else {
+//    	if (grid[0].getChildren) {
+//    		generateFunction();
+//    	}
+    }
+};
+//
+var imageRequest = function() {
+//	
+	windowScroll = window.getScroll();
+//	
+
+	
+	
+	// filter items
+	visibleGrid = grid.filter(function(el, key) {
+		elPos = el.getPosition();
+		if (elPos.y <= (window.getSize().y + windowScroll.y)) {
+			return true;
+		}
+	});
+//	
+	// each grid	
+	visibleGrid.each(function(el, i) {
+	
+		pageNumber = el.getParent('[class~=page]').getProperty('id');
+	
+		if (el.hasChildNodes() === false) {
+			var id = el.getProperty('id').split('_');
+			var row = id[0].substr(1);
+			var col = id[1].substr(1);
+			
+			var imgRequest = new Request({
+				url: '002.php',
+				onComplete: function(response) {
+					if (htmlBackgroundSize) {
+						el.setStyles({
+							'background-image':    'url('+response+')',
+							'background-repeat':   'no-repeat',
+							'background-position': 'top left',
+							'background-size':	   '100%'
+						});
+						el.set('html', '<em></em>'); // otherwise it will reload each grid on a scroll event
+					} else {
+						el.set('html', '<img src="'+response+'" style="width: 100%; height: 100%;" />');
+					}
+				
+				}
+			}).get({
+				p: pageNumber, // hardset page - only one during dev
+				r: row,
+				c: col,
+				z: zoomLevel
+			});
+		}
+	
+	});
+};
+
+
+window.addEvent('load', function() {
+	if ($$('html').hasClass('backgroundsize')) {
+		htmlBackgroundSize = true;
+	};
+
+	generateFunction();
+});
+
+// var timeout = false;
+
+window.addEvent('resize', function() {
+	//if (timeout !== false) {
+	//	clearTimeout(timeout);
+	//}
+	
+	//timeout = scaler.delay(0);
+	scaler();
+});
+
+window.addEvent('scroll', function() {
+//	imageRequest();
+});
--- playground/front_end/new_page_model/image.php
+++ playground/front_end/new_page_model/image.php
@@ -1,8 +1,7 @@
 <?php
 
-
 // crop image
-function cropImage($path, $r, $c, $z) {
+function gridCrop($path, $r, $c, $z) {
 	
 	if (!file_exists('../../new_page_model/001/cache/'.$z.'_r'.$r.'_c'.$c.'.jpg')) {
 		// image path
@@ -13,11 +12,8 @@ function cropImage($path, $r, $c, $z) {
 		
 		// create image
 		$image = imagecreatefromjpeg($imgPath) or die('test');
-		
 		$img = imagecreatetruecolor(256, 256);
-		
 		imagecopy($img, $image, 0, 0, ($c * 256), ($r * 256), 256, 256);
-	
 	
 		imagejpeg($img, '../../new_page_model/001/cache/'.$z.'_r'.$r.'_c'.$c.'.jpg', 100);
 	}
@@ -35,7 +31,6 @@ $dir  = "../../new_page_model/";
 $page = sprintf("%03s", $page); // make 3 characters, pad with 0's if needed
 $zoom = '_'.substr($zoom, 1);
 
-
 $path = $dir.$page.'/';
 
-	cropImage($path, $row, $col, $zoom);
+gridCrop($path, $row, $col, $zoom);
